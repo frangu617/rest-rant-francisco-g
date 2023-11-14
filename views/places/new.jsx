3f0c608e7d348f052fca0ac4485c6a621bda1dad
frangu617 <142462584+frangu617@users.noreply.github.com>
2023-11-13 21:39:48 -0800

    about to start part 4 in part 5

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,6 +6,29 @@ function new_form() {
     <Def>
       <main>
         <h1>Add a New Place</h1>
+        <form method="POST" action="/places">
+          <div>
+            <label htmlFor="name">Place Name</label>
+            <input type="text" id="name" name="name" required />
+          </div>
+          <div>
+            <label htmlFor="pic">Place Picture</label>
+            <input type="url" id="pic" name="pic" />
+          </div>
+          <div>
+            <label htmlFor="city">City</label>
+            <input type="text" id="city" name="city" />
+          </div>
+          <div>
+            <label htmlFor="state">State</label>
+            <input type="text" id="state" name="state" />
+          </div>
+          <div>
+            <label htmlFor="cuisines">Cuisines</label>
+            <input type="text" id="cuisines" name="cuisines" required />
+          </div>
+          <input type="submit" value="Add Place" />
+        </form>
       </main>
     </Def>
   );
