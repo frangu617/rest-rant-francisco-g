3b98f65edeb522f7d4446df72b58124ffc3519e8
frangu617 <142462584+frangu617@users.noreply.github.com>
2023-11-15 10:10:27 -0800

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,33 +1,46 @@
 const router = require('express').Router();
+const places = require('../models/places.js')
 
 router.get('/new', (req, res) => {
   res.render(`places/new`)
 })
 
 router.get('/', (req, res) => {
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/thai-food.jpg',
-    picAuthor: 'Bruna Branco',
-    picAuthorAddress: 'https://unsplash.com/@brunabranco'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/coffee-cat.jpg',
-    picAuthor: 'Ricardo L',
-    picAuthorAddress: 'https://unsplash.com/@ricardol'
-  }]
+//   let places = [{
+//     name: 'H-Thai-ML',
+//     city: 'Seattle',
+//     state: 'WA',
+//     cuisines: 'Thai, Pan-Asian',
+//     pic: '/images/thai-food.jpg',
+//     picAuthor: 'Bruna Branco',
+//     picAuthorAddress: 'https://unsplash.com/@brunabranco'
+//   }, {
+//     name: 'Coding Cat Cafe',
+//     city: 'Phoenix',
+//     state: 'AZ',
+//     cuisines: 'Coffee, Bakery',
+//     pic: '/images/coffee-cat.jpg',
+//     picAuthor: 'Ricardo L',
+//     picAuthorAddress: 'https://unsplash.com/@ricardol'
+//   }]
 
   res.render(`places/index`, { places })
 })
 router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST /places')
+  
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
+
 
 module.exports = router;
