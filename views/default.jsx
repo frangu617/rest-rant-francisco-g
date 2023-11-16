1d23dad7da7be35679f5e56f5fcd37d5c1a85b7f
frangu617 <142462584+frangu617@users.noreply.github.com>
2023-11-15 19:18:48 -0800

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -14,6 +14,19 @@ function Def(html) {
         <link rel="stylesheet" href="/css/style.css" />
       </head>
       <body>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/places">Places</a>
+            </li>
+            <li>
+              <a href="/places/new">Add Place</a>
+            </li>
+          </ul>
+        </nav>
         <div className="main-content">{html.children}</div>
         <footer className="footer">
           <div className="dropdown">
