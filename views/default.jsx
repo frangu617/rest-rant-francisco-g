3c4f408f41a8ff8b7ad8b1676295e450bd590132
frangu617 <142462584+frangu617@users.noreply.github.com>
2023-11-15 21:31:00 -0800

    Delete functionality implemented and bonus

--- views/default.jsx
+++ views/default.jsx
@@ -10,22 +10,46 @@ function Def(html) {
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
         />
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+          crossOrigin="anonymous"
+        />
         <link rel="stylesheet" href="/css/style.css" />
       </head>
       <body>
-        <nav>
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/places">Places</a>
-            </li>
-            <li>
-              <a href="/places/new">Add Place</a>
-            </li>
-          </ul>
+        <nav className="navbar sticky-top navbar-expand-lg navbar-primary bg-primary">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/places">
+                  Places
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/places/new">
+                  Add Place
+                </a>
+              </li>
+            </ul>
+          </div>
         </nav>
         <div className="main-content">{html.children}</div>
         <footer className="footer">
